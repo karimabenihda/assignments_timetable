babb445191aaccdcfbef13477152226ad1c138b7
defaltastra <defaltastra@gmail.com>
2024-11-17 16:56:12 +0100

    for my dear karimato

--- src/components/AssignmentModal.jsx
+++ src/components/AssignmentModal.jsx
@@ -1,254 +1,136 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setShowAddAssignmentModal, deleteAssignment, addAssignmentToAPI } from "../redux/slices";  
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function AssignmentModal({ groups }) {
-  const dispatch = useDispatch();
-  
-  // Accessing necessary Redux state
-  const showAddAssignmentModal = useSelector(
-    (state) => state.calendar.showAddAssignmentModal
-  );
-  const hours = useSelector((state) => state.calendar.hours);
-  const selectedDay = useSelector((state) => state.calendar.selectedDay);  // Format of selectedDay: "2024-11-11"
-  const selectedStartTime = useSelector(
-    (state) => state.calendar.selectedStartTime
-  );
-  const selectedEndTime = useSelector(
-    (state) => state.calendar.selectedEndTime
-  );
-  const assignments = useSelector((state) => state.calendar.assignments);
-  const error = useSelector((state) => state.calendar.error); // Redux error handling for the API request
-
-  const timeSlots = hours.flatMap((hour) => hour.subHours);
-
-  const existingAssignment = assignments.find(
-    (assignment) =>
-      assignment.day === selectedDay &&
-      assignment.startTime === selectedStartTime &&
-      assignment.endTime === selectedEndTime
-  );
-
+const AssignmentModal = ({ seance, isEditing, groups, onSave, onDelete, onClose }) => {
   const [assignmentData, setAssignmentData] = useState({
-    title: "",
-    teacher: "",
-    group: "",
-    nbrAssignment: "",
-    startTime: "",
-    endTime: "",
-    saleName: "",
-    day: "", // Storing the day name
-    id: null,
+    title: seance ? seance.title : "",
+    group: seance ? seance.group : "",
+    startTime: seance ? seance.startTime : "",
+    endTime: seance ? seance.endTime : "",
+    saleName: seance ? seance.saleName : "",
+    day: seance ? seance.day : "",
+    id: seance ? seance.id : null,
   });
 
   useEffect(() => {
-    if (showAddAssignmentModal && selectedStartTime && selectedEndTime) {
-      if (existingAssignment) {
-        setAssignmentData(existingAssignment);
-      } else {
-        setAssignmentData((prevData) => ({
-          ...prevData,
-          startTime: selectedStartTime,
-          endTime: selectedEndTime,
-          day: formatDateToDayName(selectedDay),  // Convert selected day to day name
-          id: null,
-        }));
-      }
+    if (seance) {
+      setAssignmentData({
+        title: seance.title,
+        group: seance.group,
+        startTime: seance.startTime,
+        endTime: seance.endTime,
+        saleName: seance.saleName,
+        day: seance.day,
+        id: seance.id,
+      });
     }
-  }, [showAddAssignmentModal, selectedStartTime, selectedEndTime, existingAssignment, selectedDay]);
+  }, [seance]);
 
-  const handleClose = () => {
-    dispatch(setShowAddAssignmentModal(false));
-    resetForm();
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setAssignmentData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Ensure `day` is set before submitting
-    const assignment = {
-      title: assignmentData.title,
-      day: assignmentData.day, // Storing day name
-      startTime: selectedStartTime,
-      endTime: selectedEndTime,
-    };
-
-    if (!assignment.day) {
-      console.error("Day is required.");
-      return;
-    }
-
-    if (assignment.title && assignment.day) {
-      dispatch(addAssignmentToAPI(assignment));
-      console.log("Submitting assignment:", assignment);
-    } else {
-      console.error("Invalid data:", assignment);
-    }
+  const handleSave = () => {
+    onSave(assignmentData);
+    onClose();  // Close modal after saving
   };
 
   const handleDelete = () => {
-    if (assignmentData.id) {
-      dispatch(deleteAssignment(assignmentData.id));
-      handleClose();
-    }
+    onDelete(assignmentData.id);
+    onClose();  // Close modal after deleting
   };
-
-  const resetForm = () => {
-    setAssignmentData({
-      title: "",
-      teacher: "",
-      group: "",
-      nbrAssignment: "",
-      startTime: "",
-      endTime: "",
-      saleName: "",
-      day: "", // Reset day name
-      id: null,
-    });
-  };
-
-  // Function to convert date string to day name
-  const formatDateToDayName = (dateString) => {
-    const date = new Date(dateString);
-    const options = { weekday: "long" }; // Options for day name (e.g., "Monday")
-    return date.toLocaleDateString("en-US", options);
-  };
-
-  if (!showAddAssignmentModal) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <form
-        className="bg-white rounded-lg shadow-2xl w-1/3 p-4"
-        onSubmit={handleSubmit}
-      >
-        <header className="flex justify-between items-center border-b pb-2 mb-2">
-          <h2 className="text-xl font-semibold">
-            {assignmentData.id ? "Edit Assignment" : "Add Assignment"}
-          </h2>
-          <button
-            type="button"
-            onClick={handleClose}
-            className="text-gray-400 hover:text-gray-600"
-          >
-            &times;
-          </button>
-        </header>
-
-        <input
-          type="text"
-          name="title"
-          placeholder="Assignment title"
-          value={assignmentData.title}
-          onChange={(e) => setAssignmentData({ ...assignmentData, title: e.target.value })}
-          className="w-full border-b-2 border-gray-300 focus:border-blue-500 outline-none py-2 text-lg"
-          required
-        />
-
-        <div className="mb-2">
-          <input
-            type="text"
-            name="saleName"
-            placeholder="Sale Name"
-            value={assignmentData.saleName}
-            onChange={(e) =>
-              setAssignmentData({ ...assignmentData, saleName: e.target.value })
-            }
-            className="w-full border-b-2 border-gray-300 focus:border-blue-500 outline-none py-2 text-lg"
-          />
-        </div>
-
-        {/* Day selection */}
-        <div className="mb-2">
-          <label htmlFor="day" className="block mb-1">
-            Select Day:
-          </label>
-          <input
-            type="text"
-            id="day"
-            value={assignmentData.day} // Display day name
-            onChange={(e) =>
-              setAssignmentData({ ...assignmentData, day: e.target.value })
-            }
-            className="w-full border-b-2 border-gray-300 focus:border-blue-500 outline-none py-2 text-lg"
-            required disabled
-          />
-        </div>
-
-        <label className="block mb-2">
-          Start Time:
-          <select
-            value={assignmentData.startTime}
-            onChange={(e) =>
-              setAssignmentData({ ...assignmentData, startTime: e.target.value })
-            }
-            className="w-full py-2 px-4 border-2 border-gray-300 rounded-lg"
-          >
-            {timeSlots.map((slot) => (
-              <option key={slot.startTime} value={slot.startTime}>
-                {slot.startTime} - {slot.endTime}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        <label className="block mb-2">
-          End Time:
-          <select
-            value={assignmentData.endTime}
-            onChange={(e) =>
-              setAssignmentData({ ...assignmentData, endTime: e.target.value })
-            }
-            className="w-full py-2 px-4 border-2 border-gray-300 rounded-lg"
-          >
-            {timeSlots.map((slot) => (
-              <option key={slot.endTime} value={slot.endTime}>
-                {slot.startTime} - {slot.endTime}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        <label className="block mb-2">
-          Group:
-          <select
-            value={assignmentData.group}
-            onChange={(e) =>
-              setAssignmentData({ ...assignmentData, group: e.target.value })
-            }
-            className="w-full py-2 px-4 border-2 border-gray-300 rounded-lg"
-            required
-          >
-            <option value="">Select a group</option>
-            {groups && groups.map((group) => (
-              <option key={group.codeGroupe} value={group.intituleGroupe}>
-                {group.intituleGroupe}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        {error && <p className="text-red-500">{error}</p>} {/* Show error messages */}
-
-        <div className="flex justify-between items-center mt-2">
-          <button
-            type="submit"
-            className="bg-sky-600 text-white px-6 py-2 rounded-full shadow-md transition-all duration-300 ease-in-out transform hover:bg-sky-400 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-sky-400"
-          >
-            {assignmentData.id ? "Update" : "Add"}
-          </button>
-
-          {assignmentData.id && (
+    <div className="modal fade show" tabIndex="-1" style={{ display: "block" }}>
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{isEditing ? "Edit Assignment" : "New Assignment"}</h5>
             <button
               type="button"
-              onClick={handleDelete}
-              className="bg-red-700 text-white px-6 py-2 rounded-full shadow-md transition-all duration-300 ease-in-out transform hover:bg-red-400 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-red-400"
-            >
-              Delete
-            </button>
-          )}
+              className="btn-close"
+              onClick={onClose}  // Close the modal
+            />
+          </div>
+          <div className="modal-body">
+            <div className="mb-3">
+              <label className="form-label">Title</label>
+              <input
+                type="text"
+                className="form-control"
+                name="title"
+                value={assignmentData.title}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Group</label>
+              <select
+                className="form-control"
+                name="group"
+                value={assignmentData.group}
+                onChange={handleInputChange}
+              >
+                {groups.length === 0 ? (
+                  <option>No groups available</option>
+                ) : (
+                  groups.map((group) => (
+                    <option key={group.codeGroupe} value={group.intituleGroupe}>
+                      {group.intituleGroupe}
+                    </option>
+                  ))
+                )}
+              </select>
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Sale Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="saleName"
+                value={assignmentData.saleName}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Start Time</label>
+              <input
+                type="time"
+                className="form-control"
+                name="startTime"
+                value={assignmentData.startTime}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">End Time</label>
+              <input
+                type="time"
+                className="form-control"
+                name="endTime"
+                value={assignmentData.endTime}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <div className="modal-footer">
+            {isEditing ? (
+              <>
+                <button className="btn btn-danger" onClick={handleDelete}>Delete</button>
+                <button className="btn btn-primary" onClick={handleSave}>Save changes</button>
+              </>
+            ) : (
+              <button className="btn btn-primary" onClick={handleSave}>Save new assignment</button>
+            )}
+          </div>
         </div>
-      </form>
+      </div>
     </div>
   );
-}
+};
+
+export default AssignmentModal;
--- src/components/Emploi_formateur.jsx
+++ src/components/Emploi_formateur.jsx
@@ -1,63 +1,247 @@
 import React, { useState, useEffect } from 'react';
-import Week from './Week.jsx';
 import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 import Logo from '../assets/Logo.svg';
 import './emploi.css';
 import AssignmentModal from './AssignmentModal.jsx';
-import axios from 'axios';
 
 function Emploi_formateur() {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const fullname = queryParams?.get('full_name');
-  
-  const [groups, setGroups] = useState([]);
-  const [seances, setSeances] = useState([]); // State for storing seances
+  const fullname = queryParams?.get('formateur');
 
-  // Fetch groups (already in your code)
+  const [groups, setGroups] = useState([]);
+  const [seances, setSeances] = useState([]);
+  const [hours, setHours] = useState([]);
+  const [scheduleMap, setScheduleMap] = useState({});
+  const [selectedSeance, setSelectedSeance] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [currentWeek, setCurrentWeek] = useState(getCurrentWeek());
+  const [filteredSeances, setFilteredSeances] = useState([]);
+
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  // Function to calculate the current week (from Sunday to Saturday)
+  function getCurrentWeek() {
+    const savedWeek = JSON.parse(localStorage.getItem('currentWeek'));
+    if (savedWeek) {
+      return savedWeek;
+    }
+    
+    const today = new Date();
+    const startOfWeek = new Date(today);
+    startOfWeek.setDate(today.getDate() - today.getDay()); // Sunday
+
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(startOfWeek.getDate() + 6); // Saturday
+
+    return {
+      start: startOfWeek,
+      end: endOfWeek,
+    };
+  }
+
   useEffect(() => {
     axios.get('http://localhost:3000/groupe')
-      .then(response => {
-        setGroups(response.data);
-      })
-      .catch(error => {
-        console.error("Error fetching groups: ", error);
-      });
+      .then(response => setGroups(response.data))
+      .catch(error => console.error("Error fetching groups:", error));
   }, []);
 
-  // Fetch seances for the selected formateur
   useEffect(() => {
-    if (fullname) {
-      axios.get(`http://localhost:8000/seances?formateur=${fullname}`)
-        .then(response => {
-          setSeances(response.data.seances); // Assuming your response contains 'seances' data
-        })
-        .catch(error => {
-          console.error("Error fetching seances: ", error);
+    axios.get('http://localhost:8000/seance')
+      .then(response => {
+        const filteredData = fullname
+          ? response.data.filter(seance => seance.formateur === fullname)
+          : response.data;
+        setSeances(filteredData);
+      })
+      .catch(error => console.error("Error fetching seances:", error));
+  }, [fullname]);
+
+  useEffect(() => {
+    setHours([
+      { startTime: '08:00', endTime: '09:00' },
+      { startTime: '09:00', endTime: '10:00' },
+      { startTime: '10:00', endTime: '11:00' },
+      { startTime: '11:00', endTime: '12:00' },
+      { startTime: '12:00', endTime: '13:00' },
+      { startTime: '13:00', endTime: '14:00' },
+      { startTime: '14:00', endTime: '15:00' },
+      { startTime: '15:00', endTime: '16:00' },
+    ]);
+  }, []);
+
+  useEffect(() => {
+    const map = {};
+
+    seances.forEach((seance) => {
+      const dayName = typeof seance.day === 'number' ? daysOfWeek[seance.day] : seance.day;
+      const group = seance.group || seance.groupe;
+      const salle = seance.saleName || seance.salle;
+
+      const key = `${dayName}-${seance.startTime}`;
+      map[key] = {
+        ...seance,
+        day: dayName,
+        group,
+        salle,
+      };
+    });
+
+    setScheduleMap(map);
+  }, [seances]);
+
+  useEffect(() => {
+    // Filter seances that are within the current week
+    const filteredData = seances.filter((seance) => {
+      const seanceDate = new Date(seance.date); // Assuming the seance has a date field
+      return seanceDate >= currentWeek.start && seanceDate <= currentWeek.end;
+    });
+    setFilteredSeances(filteredData);
+  }, [seances, currentWeek]);
+
+  const handleCellClick = (day, hour) => {
+    const key = `${day}-${hour.startTime}`;
+    const seance = scheduleMap[key];
+    if (seance) {
+      setSelectedSeance(seance);
+      setIsEditing(true);
+    } else {
+      setSelectedSeance({
+        day,
+        startTime: hour.startTime,
+        endTime: hour.endTime,
+      });
+      setIsEditing(false);
+    }
+    setShowModal(true);
+  };
+
+  const handleSaveSeance = (newSeance) => {
+    if (isEditing) {
+      axios.put(`http://localhost:8000/seance/${selectedSeance.id}`, newSeance)
+        .then(() => {
+          setSeances((prev) =>
+            prev.map((seance) =>
+              seance.id === selectedSeance.id ? newSeance : seance
+            )
+          );
+        });
+    } else {
+      axios.post('http://localhost:8000/seance', newSeance)
+        .then((response) => {
+          setSeances((prev) => [...prev, response.data]);
         });
     }
-  }, [fullname]); // Re-run the effect when fullname changes
+    setShowModal(false);
+  };
+
+  const handleDeleteSeance = (id) => {
+    axios.delete(`http://localhost:8000/seance/${id}`)
+      .then(() => {
+        setSeances((prev) => prev.filter((seance) => seance.id !== id));
+      });
+    setShowModal(false);
+  };
+
+  const goToNextWeek = () => {
+    const newStartOfWeek = new Date(currentWeek.start);
+    newStartOfWeek.setDate(newStartOfWeek.getDate() + 7);
+
+    const newEndOfWeek = new Date(currentWeek.end);
+    newEndOfWeek.setDate(newEndOfWeek.getDate() + 7);
+
+    const newWeek = {
+      start: newStartOfWeek,
+      end: newEndOfWeek,
+    };
+    setCurrentWeek(newWeek);
+    localStorage.setItem('currentWeek', JSON.stringify(newWeek));
+  };
+
+  const goToPreviousWeek = () => {
+    const newStartOfWeek = new Date(currentWeek.start);
+    newStartOfWeek.setDate(newStartOfWeek.getDate() - 7);
+
+    const newEndOfWeek = new Date(currentWeek.end);
+    newEndOfWeek.setDate(newEndOfWeek.getDate() - 7);
+
+    const newWeek = {
+      start: newStartOfWeek,
+      end: newEndOfWeek,
+    };
+    setCurrentWeek(newWeek);
+    localStorage.setItem('currentWeek', JSON.stringify(newWeek));
+  };
 
   return (
     <>
       <h6 className="ofppt">
-        <img src={Logo} alt="ofppt" style={{ width: '80px', marginLeft: "30px" }} /> <br />
-        Souss-Massa <br />
+        <img src={Logo} alt="ofppt" style={{ width: '80px', marginLeft: "30px" }} /><br />
+        Souss-Massa<br />
         ISTA AIT MELLOUL
       </h6><br />
-      <h1>
-        <strong>Emploi du Temps</strong>
-      </h1>
+      <h1><strong>Emploi du Temps</strong></h1>
       <h5>2024/2025</h5>
       <div className="items">
         <h5>
-          <b>Formateur: </b>
-          {fullname}
+          <b>Formateur:</b> {fullname || 'All formateurs'}
         </h5>
       </div>
       <div className="flex-1 p-1">
-        <Week seances={seances} /> 
-        <AssignmentModal groups={groups} />
+        <div>
+          <button className="btn btn-primary" onClick={goToPreviousWeek}>Previous Week</button>
+          <span>{`Week of ${new Date(currentWeek.start).toLocaleDateString()} - ${new Date(currentWeek.end).toLocaleDateString()}`}</span>
+          <button className="btn btn-primary" onClick={goToNextWeek}>Next Week</button>
+        </div>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th className="text-center">Time/Day</th>
+              {daysOfWeek.slice(1, 6).map((day) => (
+                <th key={day} className="text-center">{day}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {hours.map((hour, hourIndex) => (
+              <tr key={hourIndex}>
+                <td className="text-center">{hour.startTime} - {hour.endTime}</td>
+                {daysOfWeek.slice(1, 6).map((day) => {
+                  const key = `${day}-${hour.startTime}`;
+                  const seance = scheduleMap[key];
+                  return (
+                    <td
+                      key={key}
+                      className={`text-center ${seance ? 'bg-info text-white' : ''}`}
+                      onClick={() => handleCellClick(day, hour)}
+                    >
+                      {seance ? (
+                        <div>
+                          <strong>{seance.group}</strong><br />
+                          <span>{seance.title}</span><br />
+                          <span>{seance.salle}</span>
+                        </div>
+                      ) : null}
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {showModal && (
+          <AssignmentModal
+            seance={selectedSeance}
+            isEditing={isEditing}
+            groups={groups}
+            onSave={handleSaveSeance}
+            onDelete={handleDeleteSeance}
+            onClose={() => setShowModal(false)}
+          />
+        )}
       </div>
     </>
   );
